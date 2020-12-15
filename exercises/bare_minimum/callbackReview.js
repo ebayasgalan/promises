fa36e86632960bbf5daa151dcd31d00a0679508c
Ben Malamud-Roam <bmalamudroam@gmail.com>
2020-12-15 12:36:19 -0800

    implement pluckFromFirstLine and getStatusCode

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,15 +4,30 @@
 
 var fs = require('fs');
 var request = require('request');
+const https = require('https');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      var fileLines = data.toString().split('\n');
+      var firstLine = fileLines[0];
+      callback(null, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, function (error, response, body) {
+    if (error) {
+      callback(error);
+    } else {
+      callback(error, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
@@ -20,3 +35,6 @@ module.exports = {
   getStatusCode: getStatusCode,
   pluckFirstLineFromFile: pluckFirstLineFromFile
 };
+
+
+
